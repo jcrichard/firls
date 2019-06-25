1f13764af859090b8b309d12b5eb6e1a2e694e89
jcharles <jcharles.richard@gmail.com>
2019-06-26 00:16:38 +0200

    [ADD] gaussian simulate

--- firls/simulate.py
+++ firls/simulate.py
@@ -2,6 +2,14 @@ import numpy as np
 
 __SEED__ = 1234
 
+def simulate_supervised_gaussian(n, p):
+    np.random.seed(__SEED__)
+    X = np.random.normal(size=(n, p))
+    beta = np.round(np.random.normal(scale=0.3, size=(p, 1)), 1)
+    mu = (X @ beta).flatten()
+    y = np.random.normal(loc=mu) * 1.0
+    return y, X, beta
+
 
 def simulate_supervised_poisson(n, p):
     np.random.seed(__SEED__)
@@ -12,11 +20,21 @@ def simulate_supervised_poisson(n, p):
     return y, X, beta
 
 
-def simulate_supervised_negative_binomiale(n, p, r):
+def simulate_supervised_negative_binomial(n, p, r):
     np.random.seed(__SEED__)
     X = np.random.normal(size=(n, p))
     beta = np.round(np.random.normal(scale=0.3, size=(p, 1)), 1)
     mu = np.exp(X @ beta).flatten()
     p = mu / (mu + r)
     y = np.random.negative_binomial(r, p) * 1.0
+    return y, X, beta
+
+
+def simulate_supervised_binomial(n, p, r):
+    np.random.seed(__SEED__)
+    X = np.random.normal(size=(n, p))
+    beta = np.round(np.random.normal(scale=0.3, size=(p, 1)), 1)
+    mu = np.exp(X @ beta).flatten()
+    p = mu / (mu + 1)
+    y = np.random.binomial(1,p) * 1.0
     return y, X, beta
